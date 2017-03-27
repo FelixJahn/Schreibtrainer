ffded7ade808cbc7265de0ece2e67805e6360879
Felix Jahn <jahn854f@htl-ottakring.ac.at>
2017-03-27 16:12:47 +0200

    Methode Split hinzugefügt -F

--- Application_Schreibtrainer/Application_Schreibtrainer/MainWindow.xaml.cs
+++ Application_Schreibtrainer/Application_Schreibtrainer/MainWindow.xaml.cs
@@ -43,7 +43,8 @@ namespace Application_Schreibtrainer
         }
         private List<string> toStringList(string s)
         {
-            return new List<string>();
+            return s.Split().ToList<string>();
+
         }
     }
 }
